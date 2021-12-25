0757ab58800db14aecef5071cc4dc93dbe915de7
Jack Thomas <jackrthomas355@gmail.com>
2021-12-25 12:33:26 -0500

    Add initial login setup script for a debian droplet and package name changes for ror2_install

---
+++ new_sudo_no_root.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#This script is for first time set up of a new droplet on digital ocean.  But it should work for most debian based distros.
+#It requires a single $1 parameters when the script is called. 
+#This is the name of an existing user who will become the sudo and ssh access point user, while disabling root logon.
+#This script is intended to be run as root or prefixed with sudo, it does not include any sudos in it at this time.
+
+#Reassign user argument to a variable.
+someone="$1"
+
+#Add the user to the sudo group.
+adduser $someone sudo
+
+#Copy the keys from root login to our new sudo user's key file.
+mkdir -p /home/$someone/.ssh && cp /root/.ssh/authorized_keys /home/$someone/.ssh/authorized_keys
+
+#Disable root login via ssh by modifying the sshd_config
+sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+
+#Make sure the user owns the contents of their key file
+chown -R $someone:$someone /home/$someone/.ssh/* && chmod -R 0600 /home/$someone/.ssh/*
+
+###Install and setup the firewall
+
+#First check for updates, then install
+apt update
+apt install ufw
+
+#Next setup our default rules
+ufw default deny incoming
+ufw default allow outgoing
+ufw limit ssh
+
+#Lastly enable the firewall and then reload
+ufw enable
+ufw reload
--- ror2_install.sh
+++ ror2_install.sh
@@ -2,9 +2,9 @@
 #This script installs the Risk of Rain 2 dedicated server via SteamCMD alongside necessary dependencies.  It also opens ports for the game, 27015/udp, via ufw
 
 #Install steamcmd and dependencies, then forward the correct port.
-apt update
-apt install steamcmd xvfb wine-stable lib32gcc1 tmux
-ufw allow 27015/udp
+sudo apt update
+sudo apt install steamcmd xvfb wine lib32gcc-s1 tmux
+sudo ufw allow 27015/udp
 
 #Next we can install the ROR2 dedicated server via the following 'automated' command
 steamcmd +login anonymous +force_install_dir /home/ror2/ +@sSteamCmdForcePlatformType windows +app_update 1180760 +quit
